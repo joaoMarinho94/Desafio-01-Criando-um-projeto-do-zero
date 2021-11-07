15c5e89ec7878e8384081b6c3337296b355ab3b9
joaoMarinho94 <joaopaulim94@gmail.com>
2021-11-07 10:15:00 -0300

    feat: create header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,11 @@
-export default function Header() {
-  // TODO
-}
+import styles from './header.module.scss';
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <img src="/img/logo.svg" alt="logo" />
+    </header>
+  );
+};
+
+export default Header;
